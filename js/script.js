e7122915d2c8806d80da27ba58d68bfdd79659ac
trueSlav <2001idercach@gmail.com>
2023-04-20 10:05:36 +0300

    + tabs block

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
         {
             speed: 1200,
             slidesToShow: 1,
-            adaptiveHeight: true,
             prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/chevron-left-solid.png"></button>',
             nextArrow: '<button type="button" class="slick-next"><img src="images/icons/chevron-right-solid.png"></button>'
         }
