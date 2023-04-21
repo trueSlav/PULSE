198a2b3081ea0e060370d0d57cc2dfa18be27d73
trueSlav <2001idercach@gmail.com>
2023-04-21 12:42:41 +0300

    +tabs/js-jquery/user-comment +mobile

--- js/script.js
+++ js/script.js
@@ -4,7 +4,59 @@ $(document).ready(function(){
             speed: 1200,
             slidesToShow: 1,
             prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/chevron-left-solid.png"></button>',
-            nextArrow: '<button type="button" class="slick-next"><img src="images/icons/chevron-right-solid.png"></button>'
+            nextArrow: '<button type="button" class="slick-next"><img src="images/icons/chevron-right-solid.png"></button>',
+            responsive: [
+                {
+                  breakpoint: 1024,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: false,
+                    mobileFirst: true
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay:true,
+                    swipeToSlide: true,
+                    arrows: false,
+                    mobileFirst: true
+                  }
+                },
+                {
+                  breakpoint: 480,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay:true,
+                    swipeToSlide: true,
+                    arrows: false,
+                    mobileFirst: true
+                  }
+                }
+            ]
         }
     );
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog_active)', function() {
+        $(this)
+          .addClass('catalog_active').siblings().removeClass('catalog_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item){
+        $(item).each(function(i) {
+            $(this).on('click', function(e){
+                e.preventDefault();
+                $('.catalog-card__content').eq(i).toggleClass('catalog-card__content_active');
+                $('.catalog-card__information').eq(i).toggleClass('catalog-card__information_active');
+            });
+        }); 
+    }
+    toggleSlide('a.catalog-card__link');
+    toggleSlide('a.catalog-card__back');
 });
